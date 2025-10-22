77f9f8fcaaf5595fcfe8630f3a388a8d1cc4600b
Anjana Rose Joju <anjanarosejoju@gmail.com>
2025-10-22 21:44:36 +0530

    Update dbconnection.java

--- dbconnection.java
+++ dbconnection.java
@@ -1,4 +1,3 @@
-// DBConnection.java
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -6,11 +5,10 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/canteen?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "password"; // change this to your DB password
+    private static final String PASSWORD = "password"; // change to your DB password
 
     static {
         try {
-            // Load MySQL JDBC driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("MySQL Driver Registered Successfully");
         } catch (ClassNotFoundException e) {
@@ -27,7 +25,7 @@ public class DBConnection {
         } catch (SQLException e) {
             System.err.println("Failed to connect to database!");
             e.printStackTrace();
-            return null; // Return null to handle it in calling code
+            return null;
         }
     }
 }
